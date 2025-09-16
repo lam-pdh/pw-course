f5aed4198920ac8b020fea338280a493a8a3a9e7
PW - Lam Phan <lamphan290997@gmail.com>
2025-09-16 19:57:31 +0700

    flatten: replace stray gitlink with normal directory for tests/Lesson-02

---
+++ tests/Lesson-02/ex3.js
@@ -0,0 +1,11 @@
+for( i = -1; i < 2; i++ ){
+    if( i > 0 ){
+console.log( 'Giá Trị Bạn Nhập Là Số Dương' )
+}
+else if( i === 0){
+console.log( 'Giá Trị Bạn Nhập Là Số 0' )
+}
+else{
+    console.log( 'Giá Trị Bạn Nhập Là Số Âm' )
+}
+}
---
+++ tests/Lesson-02/ex4.js
@@ -0,0 +1,7 @@
+const height = 164;
+const oddHeight = height % 100;
+const minWeight = oddHeight * 8 / 10; 
+const idealWeight =  oddHeight * 9 / 10;
+const maxWeight = oddHeight;
+
+console.log( idealWeight, maxWeight, minWeight )
